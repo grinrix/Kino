a8a5fd469615fc4b96759ffaaf58ddcd0cdcdb4d
grinrix <146460587+grinrix@users.noreply.github.com>
2026-01-16 20:59:20 +0100

    dodano:  SeatMapService  Show

--- src/cinema/domain/Show.java
+++ src/cinema/domain/Show.java
@@ -1,4 +1,46 @@
 package cinema.domain;
+import java.util.List;
 
-public class Show {
+public class Show implements Exportable {
+    private  String movieTitle;
+    private  Auditorium auditorium;
+    private  String startTime;
+    private  List<Seat> seats;
+    private  String imagePath; //dla plakatów do filmów
+    public  Show(String movieTitle, Auditorium auditorium, String startTime, String imagePath) {
+        this.movieTitle = movieTitle;
+        this.auditorium = auditorium;
+        this.startTime = startTime;
+        this.imagePath = imagePath;
+        this.seats=SeatMapService.generateSeats(auditorium); // miejsca są automatycznie wygenerowane podczas tworzenie show
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+    public Auditorium getAuditorium() {
+        return auditorium;
+    }
+    public List<Seat> getSeats() {
+        return seats;
+    }
+    public String getStartTime() {
+        return startTime;
+    }
+    public String getImagePath() {
+        return imagePath;
+    }
+    @Override
+    public String toCsv() {
+        long occupiedCount = seats.stream().filter(seat -> !seat.isAvailable()).count(); // ile jest zajętych
+        double price = (auditorium instanceof IMAXAuditorium) ? 35.00 : 20.00; // IMAX jest droższy
+        double revenue = occupiedCount * price;
+        return String.format("%s,%s,%s,%d,%.2f",
+                movieTitle,
+                auditorium.getName(),
+                startTime,
+                occupiedCount,
+                revenue
+        );
+    }
 }
--- src/cinema/service/SeatMapService.java
+++ src/cinema/service/SeatMapService.java
@@ -1,4 +1,17 @@
 package cinema.service;
+import cinema.domain.Auditorium;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SeatMapService {
+    public static List<Seat> generateSeats(Auditorium auditorium) {
+        List<Seat> seats = new ArrayList<>();
+        // robimy pętle do stworzenia listy miejsc
+        for (int n = 1; n <=auditorium.getRows(); n++) {
+            for(int m = 1; n <=auditorium.getSeatsPerRow(); m++) {
+                seats.add(new Seat(n,m));
+            }
+        }
+        return  seats;
+    }
 }
