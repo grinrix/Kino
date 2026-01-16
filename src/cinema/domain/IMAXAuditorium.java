6475749e26bab68ed5791895eb3f25483a0f391f
grinrix <146460587+grinrix@users.noreply.github.com>
2026-01-16 21:06:45 +0100

    dodano: opisy TODO do brakujących plików

--- src/cinema/domain/IMAXAuditorium.java
+++ src/cinema/domain/IMAXAuditorium.java
@@ -1,4 +1,5 @@
 package cinema.domain;
 
 public class IMAXAuditorium {
+    // TODO: wzorując się klasą abstrakcyjną użyć super() do przekazania danych do konstruktora nadklasy
 }
--- src/cinema/service/TicketService.java
+++ src/cinema/service/TicketService.java
@@ -1,4 +1,5 @@
 package cinema.service;
 
 public class TicketService {
+    //TODO: reserveTicket (show, row seatNumber) który: szuka miejsca danego seansu, sprawdza czy takie miejsce istnieje oraz na koniec wykonuje próbe rezerwacji miejsca
 }
